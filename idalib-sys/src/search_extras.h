c98a15ede2b9e8c30990f9500c98c678a7c4cf41
raptor <raptor@0xdeadbeef.info>
2024-11-11 17:38:27 +0100

    Clean up idalib_find_text()

--- idalib-sys/src/search_extras.h
+++ idalib-sys/src/search_extras.h
@@ -6,15 +6,4 @@
 
 ea_t idalib_find_text(ea_t start_ea, const char *text) {
   return find_text(start_ea, 0, 0, text, SEARCH_DOWN | SEARCH_NEXT);
-
-  // TODO
-  //auto cmt = qstring();
-
-  //if (get_cmt(&cmt, ea, rptble) != 0) {
-    //return rust::String(cmt.c_str());
-  //} else {
-    //return rust::String();
-  //}
-
-  //return BADADDR;
 }
