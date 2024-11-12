a144c5c28647687dadfd21047fb63226959140ca
raptor <raptor@0xdeadbeef.info>
2024-11-12 12:37:50 +0100

    Add find_imm_all() and do further testing

--- idalib/src/idb.rs
+++ idalib/src/idb.rs
@@ -313,13 +313,11 @@ impl IDB {
 
     pub fn find_text_all(&self, text: impl AsRef<str>) -> Option<Vec<Address>> {
         let mut v = vec![];
-
-        let mut cur = self.meta().start_code_segment();
+        let mut cur = 0u64.into();
         while let Some(found) = self.find_text(cur, text.as_ref()) {
             v.push(found);
             cur = self.find_defined(found).unwrap_or(BADADDR.into());
         }
-
         if v.is_empty() {
             None
         } else {
@@ -327,13 +325,26 @@ impl IDB {
         }
     }
 
-    // TODO: test, add `find_imm_all()`?
     pub fn find_imm(&self, start_ea: Address, imm: u32) -> Option<Address> {
         let addr = unsafe { idalib_find_imm(start_ea.into(), imm.into()) };
         if addr == BADADDR {
             None
         } else {
             Some(addr.into())
+        }
+    }
+
+    pub fn find_imm_all(&self, imm: u32) -> Option<Vec<Address>> {
+        let mut v = vec![];
+        let mut cur = 0u64.into();
+        while let Some(found) = self.find_imm(cur, imm) {
+            v.push(found);
+            cur = found;
+        }
+        if v.is_empty() {
+            None
+        } else {
+            Some(v)
         }
     }
 
