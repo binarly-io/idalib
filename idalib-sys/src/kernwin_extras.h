4ff998e211fdca08448db64c11533ecb2587d7b6
yeggor <vasilenko.yegor@gmail.com>
2024-11-13 00:38:09 +0000

    extend license_manager_t structure

--- idalib-sys/src/kernwin_extras.h
+++ idalib-sys/src/kernwin_extras.h
@@ -16,18 +16,40 @@ struct license_result_t {
   uint16_t _skip;
   uint32_t is_ok;
   uint32_t pidx;
+  uint32_t eidx;
 };
 
-struct license_config_t {
+struct license_location_t {
+  qstring server_host;
+  uint16_t server_port;
+  uint8_t _skip_a;
+  uint8_t _skip_b;
+  uint32_t _skip_c;
+  uint64_t remote;
+  qstring license_path;
+};
+
+struct license_info_t {
   uint8_t lid[6];
   uint16_t _skip;
   uint32_t pidx;
   uint32_t eidx;
 };
 
+struct license_addon_info_t {
+  uint8_t lid[6];
+  uint16_t _skip_a;
+  uint32_t aidx;
+  uint8_t owner_lid[6];
+  uint16_t _skip_b;
+  uint32_t _skip_c;
+  uint64_t start_date;
+  uint64_t end_date;
+};
+
 struct license_manager_t_vtbl {
   void *_skip_a[4];
-  int (*get_or_borrow_license)(license_manager_t *, void *, license_config_t *,
+  int (*get_or_borrow_license)(license_manager_t *, void *, license_info_t *,
                                uint64_t, qstring *);
   void *(*get_license_location)(license_manager_t *);
   void *_skip_b[5];
@@ -36,6 +58,25 @@ struct license_manager_t_vtbl {
 
 struct license_manager_t {
   license_manager_t_vtbl *_vtbl;
+  qvector<qstring> ida_dirs;
+  void *logger;
+  license_location_t license_location;
+  qvector<license_info_t> licenses;
+  qstring user_name;
+  qstring user_email;
+  license_result_t result;
+  uint32_t _skip_a[3];
+  qstring owner;
+  qvector<license_addon_info_t> addons;
+  void *_skip_b[3];
+  uint64_t start_date;
+  uint64_t end_date;
+  uint64_t issued_on;
+  qstring description;
+  uint64_t _skip_c;
+  qstring license_content;
+  uint64_t _skip_d[16];
+  qstring machine_id;
 };
 
 extern "C" license_manager_t *get_license_manager();
@@ -52,7 +93,7 @@ bool idalib_check_license() {
   }
 
   auto license_location = manager->_vtbl->get_license_location(manager);
-  license_config_t license_config = {{0}, 0, 1, 1};
+  license_info_t license_config = {{0}, 0, 1, 1};
   uint64_t flags = 16;
 
   // NOTE: this will contain a description of any error; we should likely
