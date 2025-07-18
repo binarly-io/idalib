e383d4588ac0d629b4b9241051ca9182a778532b
Sam L. Thomas <s@ghost.sh>
2025-07-18 17:47:19 +0100

    initial work on v9.2 support

--- idalib-sys/src/bookmarks_extras.h
+++ idalib-sys/src/bookmarks_extras.h
@@ -5,18 +5,20 @@
 #include "cxx.h"
 
 rust::u32 idalib_bookmarks_t_mark(ea_t ea, uint32 index, const char *desc) {
+  auto widget = qstring();
   idaplace_t ipl(ea, 0);
   renderer_info_t rinfo;
   rinfo.rtype = TCCRT_FLAT;
   rinfo.pos.cx = 0;
   rinfo.pos.cy = 5;
-  lochist_entry_t e(&ipl, rinfo);
+  lochist_entry_t e(&ipl, rinfo, widget);
 
   return bookmarks_t_mark(e, index, nullptr, desc, nullptr);
 }
 
 rust::String idalib_bookmarks_t_get_desc(uint32 index) {
   auto desc = qstring();
+  auto widget = qstring();
   ea_t ea = 0;
 
   idaplace_t ipl(ea, 0);
@@ -24,7 +26,7 @@ rust::String idalib_bookmarks_t_get_desc(uint32 index) {
   rinfo.rtype = TCCRT_FLAT;
   rinfo.pos.cx = 0;
   rinfo.pos.cy = 5;
-  lochist_entry_t e(&ipl, rinfo);
+  lochist_entry_t e(&ipl, rinfo, widget);
 
   if (bookmarks_t_get_desc(&desc, e, index, nullptr) != 0) {
     return rust::String(desc.c_str());
@@ -35,6 +37,7 @@ rust::String idalib_bookmarks_t_get_desc(uint32 index) {
 
 ea_t idalib_bookmarks_t_get(uint32 index) {
   auto desc = qstring();
+  auto widget = qstring();
   ea_t ea = 0;
 
   idaplace_t ipl(ea, 0);
@@ -42,7 +45,7 @@ ea_t idalib_bookmarks_t_get(uint32 index) {
   rinfo.rtype = TCCRT_FLAT;
   rinfo.pos.cx = 0;
   rinfo.pos.cy = 5;
-  lochist_entry_t e(&ipl, rinfo);
+  lochist_entry_t e(&ipl, rinfo, widget);
 
   lochist_entry_t loc(e);
 
@@ -54,6 +57,7 @@ ea_t idalib_bookmarks_t_get(uint32 index) {
 }
 
 bool idalib_bookmarks_t_erase(uint32 index) {
+  auto widget = qstring();
   ea_t ea = 0;
 
   idaplace_t ipl(ea, 0);
@@ -61,23 +65,25 @@ bool idalib_bookmarks_t_erase(uint32 index) {
   rinfo.rtype = TCCRT_FLAT;
   rinfo.pos.cx = 0;
   rinfo.pos.cy = 5;
-  lochist_entry_t e(&ipl, rinfo);
+  lochist_entry_t e(&ipl, rinfo, widget);
 
   return bookmarks_t_erase(e, index, nullptr);
 }
 
 rust::u32 idalib_bookmarks_t_find_index(ea_t ea) {
+  auto widget = qstring();
   idaplace_t ipl(ea, 0);
   renderer_info_t rinfo;
   rinfo.rtype = TCCRT_FLAT;
   rinfo.pos.cx = 0;
   rinfo.pos.cy = 5;
-  lochist_entry_t e(&ipl, rinfo);
+  lochist_entry_t e(&ipl, rinfo, widget);
 
   return bookmarks_t_find_index(e, nullptr);
 }
 
 rust::u32 idalib_bookmarks_t_size(void) {
+  auto widget = qstring();
   ea_t ea = 0;
 
   idaplace_t ipl(ea, 0);
@@ -85,7 +91,7 @@ rust::u32 idalib_bookmarks_t_size(void) {
   rinfo.rtype = TCCRT_FLAT;
   rinfo.pos.cx = 0;
   rinfo.pos.cy = 5;
-  lochist_entry_t e(&ipl, rinfo);
+  lochist_entry_t e(&ipl, rinfo, widget);
 
   return bookmarks_t_size(e, nullptr);
 }
